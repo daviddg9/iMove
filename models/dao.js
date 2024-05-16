edc4eb100b396d5957c9835132c70e546aebd70a
daviddg9 <david.diezg9@gmail.com>
2024-05-16 18:21:31 +0200

    Cambios

--- models/dao.js
+++ models/dao.js
@@ -5,6 +5,8 @@ class DAO {
     dato1 = "";
     dato2 = "hola";
     dato3 = 42;
+    title = "¿Dónde quieres viajar?";
+    lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
 
     // Constructor
     constructor(dato1) {
