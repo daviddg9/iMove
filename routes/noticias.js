2fe2dd991e661fcdd2312a151b1a969987ad3a4b
ddiez2 <ddiez2@seidor.es>
2024-05-30 20:00:51 +0200

    Estructuración general de Templates

--- routes/noticias.js
+++ routes/noticias.js
@@ -5,17 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('noticias', {
       layout: 'noticias',
-      title: "Nuestras noticias", 
+      title: "noticias", 
       titleNoticia: 'Innovaciones tecnológicas en el transporte público en 2023', 
-      noticia: 'En 2023, el transporte público se enfrenta a numerosos desafíos que están siendo abordados mediante innovaciones tecnológicas. Desde la implementación de sistemas de pago sin contacto hasta la incorporación de vehículos eléctricos y autónomos, las ciudades están adoptando soluciones tecnológicas para mejorar la eficiencia y la sostenibilidad del transporte público ',
-      
-    
-    
-    
-    
-    
-    
-    
+      noticia: 'En 2023, el transporte público se enfrenta a numerosos desafíos que están siendo abordados mediante innovaciones tecnológicas. Desde la implementación de sistemas de pago sin contacto hasta la incorporación de vehículos eléctricos y autónomos, las ciudades están adoptando soluciones tecnológicas para mejorar la eficiencia y la sostenibilidad del transporte público '
     });
   });
 
